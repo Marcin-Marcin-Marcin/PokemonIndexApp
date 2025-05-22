fb9b04e95a7a0647c3c7843c5137a1930a3fb719
MarcinBiesiada <mtbiesiada@gmail.com>
2025-05-22 16:56:22 +0200

    Pokemon List added to scripts.js
    If implemented this commit would have added a list of pokemons to the scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-alert('Hello world');
-
-let favoriteFood = 'Steak';
-
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'bulbasaur', height: 7, type: ['grass','poison']},
+    {name: 'gothitelle', height: 1.5, type: 'psychic'},
+    {name: 'lugia', height: 5.2, type: ['psychic','flying']},
+    {name: 'mewtwo', height: 2, type: 'psychic'},
+    {name: 'gardevoir', height: 1.6, type: ['psychic','fairy']},
+    {name: 'furret', height: 1.8, type: 'normal'},
+    {name: 'jynx', height: 1.4, type: ['psychic','ice']},
+    {name: 'milotic', height: 2, type: 'water'}
+    {name: 'swanna', height: 1.3, type: ['water','flying']}
+];
