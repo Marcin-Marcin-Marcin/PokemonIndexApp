4dda2e7202ca32de3db9b74e6ac5e7b872b5b686
MarcinBiesiada <mtbiesiada@gmail.com>
2025-05-23 17:30:32 +0200

    Loop printing list of Pokemons
    If accepted this commit would have printed the list of Pokemons on the webpage and highlighted the largest Pokemon in the list.

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,28 @@ let pokemonList = [
     {name: 'gardevoir', height: 1.6, type: ['psychic','fairy']},
     {name: 'furret', height: 1.8, type: 'normal'},
     {name: 'jynx', height: 1.4, type: ['psychic','ice']},
-    {name: 'milotic', height: 2, type: 'water'}
+    {name: 'milotic', height: 2, type: 'water'},
     {name: 'swanna', height: 1.3, type: ['water','flying']}
 ];
+
+/*First version
+// Printing out the details of each Pokemon
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height);
+}
+
+//Highlighting the largest Pokemon
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 6) {
+     document.write('<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height + " wow, that's big!");
+    }
+}*/
+
+//Second Version
+for (let i = 0; i < pokemonList.length; i++) {
+let message = '<p>' + pokemonList[i].name + ' height ' + pokemonList[i].height;
+  if (pokemonList[i].height > 6) {
+    message += " wow, that's big!";
+  }
+  document.write(message);
+}
