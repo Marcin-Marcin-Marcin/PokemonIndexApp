38cf71c1f9aa582e9b00b5879eae3d7b3f7163c2
MarcinBiesiada <mtbiesiada@gmail.com>
2025-05-30 13:30:50 +0200

    DOM Manipulation for Pokemon List
    If applied, this commit would have replaced document.write with DOM manipulation.

--- js/scripts.js
+++ js/scripts.js
@@ -59,16 +59,48 @@ let pokemonList = [
     {name: 'milotic', height: 2, type: ['water']},
     {name: 'swanna', height: 1.3, type: ['water','flying']}
 ];
-return {
-  add: function(pokemon) {
-  pokemonList.push(pokemon);
-},
-getAll: function() {
-  return pokemonList;
-}
-};
-}) ();
 
+function addListItem(pokemon) {
+    let pokemonListElement = document.querySelector('.pokemon-list');
+    let listItem = document.createElement('li');
+    let button = document.createElement('button');
+
+    button.innerText = pokemon.name;
+    button.classList.add('pokemon-button');
+
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    });
+
+    listItem.appendChild(button);
+    pokemonListElement.appendChild(listItem);
+  }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  };
+
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
+
+/*
 //Version with forEach function adapted for pokemonRepository
 pokemonRepository.getAll().forEach(function (pokemon) {
 let message = `<p>${pokemon.name}: ${pokemon.height}`;
@@ -78,4 +110,4 @@ let message = `<p>${pokemon.name}: ${pokemon.height}`;
   message += '</p>';
   document.write(message);
 }
-);
+);*/
