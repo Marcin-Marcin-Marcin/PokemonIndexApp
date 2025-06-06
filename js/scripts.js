fe3879577aef6da2b94feee03416e60876c4f92f
MarcinBiesiada <mtbiesiada@gmail.com>
2025-06-06 16:49:12 +0200

    API data loading
    If implemented, this commit would have replaced the static pokemon list with dynamic API data loading, added loadList, loadDetails, and included polyfills.

--- js/scripts.js
+++ js/scripts.js
@@ -46,21 +46,43 @@ let message = `<p>${pokemon.name}: ${pokemon.height}`;
 }
 );*/
 
+// Optional loading indicator
+function showLoadingMessage() {
+  let loadingMessage = document.createElement('p');
+  loadingMessage.innerText = 'Loading...';
+  loadingMessage.id = 'loading-message';
+  document.body.appendChild(loadingMessage);
+}
+
+function hideLoadingMessage() {
+  let loadingMessage = document.querySelector('#loading-message');
+  if (loadingMessage) {
+    loadingMessage.remove();
+  }
+}
+
 //Pokemon List using IIFE
 let pokemonRepository = (function(){
-let pokemonList = [
-    {name: 'bulbasaur', height: 7, type: ['grass','poison']},
-    {name: 'gothitelle', height: 1.5, type: ['psychic']},
-    {name: 'lugia', height: 5.2, type: ['psychic','flying']},
-    {name: 'mewtwo', height: 2, type: ['psychic']},
-    {name: 'gardevoir', height: 1.6, type: ['psychic','fairy']},
-    {name: 'furret', height: 1.8, type: ['normal']},
-    {name: 'jynx', height: 1.4, type: ['psychic','ice']},
-    {name: 'milotic', height: 2, type: ['water']},
-    {name: 'swanna', height: 1.3, type: ['water','flying']}
-];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-function addListItem(pokemon) {
+  function add(pokemon) {
+    if (
+      typeof pokemon === 'object' &&
+      'name' in pokemon &&
+      'detailsUrl' in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      console.log('Invalid Pokémon:', pokemon);
+    }
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  function addListItem(pokemon) {
     let pokemonListElement = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
     let button = document.createElement('button');
@@ -76,28 +98,62 @@ function addListItem(pokemon) {
     pokemonListElement.appendChild(listItem);
   }
 
+  function loadList() {
+    showLoadingMessage();
+    return fetch(apiUrl).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+      hideLoadingMessage();
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    });
+  }
+
+  function loadDetails(pokemon) {
+    showLoadingMessage();
+    return fetch(pokemon.detailsUrl).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      pokemon.imageUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.types = details.types;
+      hideLoadingMessage();
+    }).catch(function (e) {
+      console.error(e);
+      hideLoadingMessage();
+    });
+  }
+
   function showDetails(pokemon) {
-    console.log(pokemon);
-  }
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  function getAll() {
-    return pokemonList;
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
 
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    showDetails: showDetails
   };
 
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+// Load list from API and render
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
 
 /*
